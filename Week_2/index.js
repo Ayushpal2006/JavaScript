f537a54fefd3a8042db705a6d78c63b31f95d7b4
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2024-01-18 06:48:40 +0530

    Added by Ayush

--- Week_2/index.js
+++ Week_2/index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 function CalculateSum(counter){
     let sum=0;
-    for(let i=0;i<counter;i++){
+    for(let i=0;i<=counter;i++){
         sum =sum+i;
     }
 
@@ -12,17 +12,22 @@ function CalculateSum(counter){
 }
 
 function handleRequest(req, res){
-    let CalculatedSum=CalculateSum(100);
-    console.log(CalculatedSum);
-    let answer="This is the sum of first 100 number  : "+CalculatedSum;
+    let counter=req.query.counter;   //give request to browser;
+    let CalculatedSum=CalculateSum(counter);
+    let answer="This is the sum of the number  : "+CalculatedSum;
     res.send(answer)
-  }
+}
 
-app.get('/', handleRequest)
+function CreateUser(req,res){
+  res.send("Hello World")
+}
+
+
+app.get('/:username', handleRequest); 
+app.post('/CreateUser',CreateUser)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 
 
